e50e522ce33583532e5073fa3a18041caf07072d
tomual <tomual@users.noreply.github.com>
2018-06-27 23:08:03 -0500

    Search interface overlay on Q + W

--- application/views/footer.php
+++ application/views/footer.php
@@ -15,7 +15,14 @@
         <div class="shortcut-label">Ticket List</div>
     </div>
 </div>
-<div class="dim" style="display:none"></div>
+<div class="search-overlay" style="display:none">
+    <div class="form-group">
+        <label class="form-label">Search</label>
+        <select class="selectize form-control"></select>
+    </div>
+</div>
+<div class="shortcuts-dim" style="display:none"></div>
+<div class="search-dim" style="display:none"></div>
 <script src="<?php echo base_url('assets/js/vendors/jquery-3.2.1.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/vendors/jquery-ui.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/js/vendors/bootstrap.bundle.min.js') ?>"></script>
@@ -35,7 +42,7 @@
     });
 
     var urlNewTicket = '<?php echo base_url('ticket/new') ?>';
-    var urlNewTicket = '<?php echo base_url('ticket/all') ?>';
+    var urlTicketList = '<?php echo base_url('ticket/all') ?>';
     var keyPressed = {};
     document.addEventListener('keydown', function (e) {
 
@@ -54,22 +61,31 @@
     }, false);
     document.addEventListener('keyup', function (e) {
         $('.shortcuts-overlay').fadeOut('fast');
-        $('.dim').fadeOut();
+        $('.shortcuts-dim').fadeOut();
     }, false);
 
     document.addEventListener('keyup', function (e) {
         keyPressed[e.key + e.location] = false;
-
         keyPressed = {};
     }, false);
 
+    $('.search-dim').on('click', function() {
+        closeSearch();
+    });
+
     function checkShortcut() {
         if (keyPressed.q0) {
-            $('.shortcuts-overlay').fadeIn('fast');
-            $('.dim').fadeIn();
+            if(!searchIsOpen()) {
+                $('.shortcuts-overlay').fadeIn('fast');
+                $('.shortcuts-dim').fadeIn();
+            }
         }
         if (keyPressed.q0 && keyPressed.w0) {
-            // window.location.href = urlNewTicket;
+            $('.search-overlay').fadeOut(function() {
+                $('.search-dim').fadeIn();
+                $('.search-overlay').fadeIn();
+            });
+            $('.shortcuts-dim').fadeOut();
         }
         if (keyPressed.q0 && keyPressed.e0) {
             window.location.href = urlNewTicket;
@@ -77,6 +93,17 @@
         if (keyPressed.q0 && keyPressed.r0) {
             window.location.href = urlTicketList;
         }
+        if (keyPressed.Escape0) {
+            closeSearch();
+        }
+    }
+
+    function searchIsOpen() {
+        return $('.search-overlay:visible').length;
+    }
+    function closeSearch() {
+        $('.search-overlay').fadeOut();
+        $('.search-dim').fadeOut();
     }
 </script>
 </body>
